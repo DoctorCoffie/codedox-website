35ec7ac8213f9b090881a99f7d81dfd7397f34e2
Bekaaaaaaa <72948584+Bekaaaaaaa@users.noreply.github.com>
2026-01-21 16:56:48 +0200

    Create emailtemp.js

---
+++ emailtemp.js
@@ -0,0 +1,17 @@
+<div style="font-family: system-ui, sans-serif, Arial; font-size: 14px; color: #2c3e50;">
+  <h2>Nova poruka sa kontakt forme</h2>
+  <p>Detalji su:</p>
+
+  <ul style="list-style: none; padding: 0;">
+    <li><strong>Ime i Prezime:</strong> {{name}}</li>
+    <li><strong>Email:</strong> {{email}}</li>
+    <li><strong>Telefon:</strong> {{phone}}</li>
+    <li><strong>Vozilo:</strong> {{car}}</li>
+    <li><strong>Usluga:</strong> {{service}}</li>
+    <li><strong>Poruka:</strong><br/>{{message}}</li>
+  </ul>
+
+  <p style="margin-top: 20px; font-size: 12px; color: #888;">
+    Poslato sa vaše web stranice
+  </p>
+</div>
